f2264ece93814524c18d96e0ca9e55c9e469bcff
AmaurySimondet <amaury.simondet@hotmail.com>
2022-09-29 16:19:23 +0200

    redesigning navbar for mobiles

--- client/src/components/NavigBar.jsx
+++ client/src/components/NavigBar.jsx
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 
 function NavigBar(){
     const [gearIsClicked, setGearIsClicked] = useState(false);
+    const [onMobile, setOnMobile] = useState(false);
+    const [toggled, setToggled] = useState(false);
 
     function handleClick(){
         setGearIsClicked(oldGear => {
@@ -9,70 +11,142 @@ function NavigBar(){
         })
     }
 
+    function toggling(){
+        setToggled(oldToggled => {
+            return !oldToggled
+        })
+    }
+
+      React.useEffect(() => {
+        function handleResize() {
+          if (window.innerWidth<500){setOnMobile(true)}
+          else{setOnMobile(false)}
+        }
+
+        window.addEventListener('resize', handleResize)
+      })
+
     return(
-    <div>
-        <nav className="navbar navbar-expand navbar-light bg-light navigbar">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-            <a className="navbar-brand" href="/dashboard"><img className="logo-navbar" src={require('../images/icons/logo-navbar.png')} alt="logo" /></a>
-            <a className="navbar-brand" href="/dashboard"><h1 className="ProgrArmor">ProgrArmor</h1></a>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div>
+        {
+        onMobile ?
+                <nav className="navbar navbar-expand navbar-light bg-light navigbar">
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                    <a className="navbar-brand" href="/dashboard"><img className="logo-navbar" src={require('../images/icons/logo-navbar.png')} alt="logo" /></a>
+                    <a className="navbar-brand" href="/dashboard"><h1 className="ProgrArmor">ProgrArmor</h1></a>
+                  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <img className="logo-navbar" onClick={toggling} src={require('../images/icons/logo-navbar.png')} alt="logo" />
+                    {toggled ?
+                        (
+                            <div>
+                                <div className="gear-is-clicked"></div>
 
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0 navbar-elements">
-              <li className="nav-item active">
-                <a className="nav-link" href="/dashboard"><img className="icon-navbar" src={require('../images/icons/home.png')} alt='home' /></a>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="/session"><img className="icon-navbar" src={require('../images/icons/write.png')} alt='session' /></a>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="/stats"><img className="icon-navbar" src={require('../images/icons/chart.png')} alt='stats' /></a>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="/programme"><img className="icon-navbar" src={require('../images/icons/plus.png')} alt='programme' /></a>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="/social"><img className="icon-navbar" src={require('../images/icons/social.png')} alt='social' /></a>
-              </li>
-              <li className="nav-item active">
-                <a onClick={handleClick} className="nav-link"><img className="icon-navbar" src={require('../images/icons/gear.png')} alt='parametres' /></a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        {gearIsClicked ? (
+                                <table className="param-choice-div">
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <a className="nav-link" href="/dashboard"><img className="icon-navbar" src={require('../images/icons/home.png')} alt='home' /></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a className="nav-link" href="/session"><img className="icon-navbar" src={require('../images/icons/write.png')} alt='session' /></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a className="nav-link" href="/stats"><img className="icon-navbar" src={require('../images/icons/chart.png')} alt='stats' /></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a className="nav-link" href="/programme"><img className="icon-navbar" src={require('../images/icons/plus.png')} alt='programme' /></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a className="nav-link" href="/social"><img className="icon-navbar" src={require('../images/icons/social.png')} alt='social' /></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <a onClick={handleClick} className="nav-link"><img className="icon-navbar" src={require('../images/icons/gear.png')} alt='parametres' /></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        )
+                    : null}
+                  </div>
+                </nav>
+        :
             <div>
-                <div className="gear-is-clicked"></div>
+                <nav className="navbar navbar-expand navbar-light bg-light navigbar">
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                    <a className="navbar-brand" href="/dashboard"><img className="logo-navbar" src={require('../images/icons/logo-navbar.png')} alt="logo" /></a>
+                    <a className="navbar-brand" href="/dashboard"><h1 className="ProgrArmor">ProgrArmor</h1></a>
+                  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0 navbar-elements">
+                          <li className="nav-item active">
+                            <a className="nav-link" href="/dashboard"><img className="icon-navbar" src={require('../images/icons/home.png')} alt='home' /></a>
+                          </li>
+                          <li className="nav-item active">
+                            <a className="nav-link" href="/session"><img className="icon-navbar" src={require('../images/icons/write.png')} alt='session' /></a>
+                          </li>
+                          <li className="nav-item active">
+                            <a className="nav-link" href="/stats"><img className="icon-navbar" src={require('../images/icons/chart.png')} alt='stats' /></a>
+                          </li>
+                          <li className="nav-item active">
+                            <a className="nav-link" href="/programme"><img className="icon-navbar" src={require('../images/icons/plus.png')} alt='programme' /></a>
+                          </li>
+                          <li className="nav-item active">
+                            <a className="nav-link" href="/social"><img className="icon-navbar" src={require('../images/icons/social.png')} alt='social' /></a>
+                          </li>
+                          <li className="nav-item active">
+                            <a onClick={handleClick} className="nav-link"><img className="icon-navbar" src={require('../images/icons/gear.png')} alt='parametres' /></a>
+                          </li>
+                        </ul>
+                  </div>
+                </nav>
 
-                <table className="param-choice-div">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <a className="param-choice" href="/compte"> Compte </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a className="param-choice" href="/aide"> Aide </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a className="param-choice" href="/a_propos"> A propos </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a className="param-choice" href="/CGU"> CGU </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                {gearIsClicked ? (
+                    <div>
+                        <div className="gear-is-clicked"></div>
+
+                        <table className="param-choice-div">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <a className="param-choice" href="/compte"> Compte </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a className="param-choice" href="/aide"> Aide </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a className="param-choice" href="/a_propos"> A propos </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <a className="param-choice" href="/CGU"> CGU </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                ) : null}
             </div>
-        ) : null}
-    </div>
+        }
+        </div>
     )
 }
 
