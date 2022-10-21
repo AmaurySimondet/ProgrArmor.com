2a3ba61c28220fbf69e7d991bd867d3cf0d144ce
AmaurySimondet <amaury.simondet@hotmail.com>
2022-10-21 12:04:27 +0200

    categorie plus dépendante de l'index (yeees)

--- controllers/account/lib.js
+++ controllers/account/lib.js
@@ -444,6 +444,7 @@ async function workouts(req, res) {
                     })
 
                     //TRI CATEGORIE
+                    let del = true;
                     for(let i=0; i<5; i++){
                         let catName = "categorie"+i+"name";
                         let catInput = "categorie"+i+"input";
@@ -452,13 +453,15 @@ async function workouts(req, res) {
                                 seances.map((seance,indexSeance) => {
                                     return (seance.exercices.map((exercice, indexExercice) => {
                                         if(exercice.Categories && Object.entries(exercice.Categories ).length !== 0){
-                                            return(Object.values(exercice.Categories).map((categorie, indexCategorie) => {
-                                                if (indexCategorie === i){
-                                                    if (categorie.name !== req.query[catName] || categorie.input !== req.query[catInput]){
-                                                        delete seances[indexSeance].exercices[indexExercice]
-                                                    }
+                                            del = true
+                                            Object.values(exercice.Categories).map((categorie, indexCategorie) => {
+                                                if (categorie.name === req.query[catName] && categorie.input === req.query[catInput]){
+                                                   del = false
                                                 }
-                                            }))
+                                            })
+                                            if (del){
+                                                delete seances[indexSeance].exercices[indexExercice]
+                                            }
                                         }
                                         else{ delete seances[indexSeance].exercices[indexExercice] }
                                     }))
@@ -469,13 +472,15 @@ async function workouts(req, res) {
                                 seances.map((seance,indexSeance) => {
                                     return (seance.exercices.map((exercice, indexExercice) => {
                                         if(exercice.Categories && Object.entries(exercice.Categories ).length !== 0){
-                                            return(Object.values(exercice.Categories).map((categorie, indexCategorie) => {
-                                                if (indexCategorie === i){
-                                                    if (categorie.name !== req.query[catName] || categorie.utilisation !== req.query[catUtilisation]){
-                                                        delete seances[indexSeance].exercices[indexExercice]
+                                                del = true
+                                                Object.values(exercice.Categories).map((categorie, indexCategorie) => {
+                                                    if (categorie.name === req.query[catName] && categorie.utilisation === req.query[catUtilisation]){
+                                                       del = false
                                                     }
+                                                })
+                                                if (del){
+                                                    delete seances[indexSeance].exercices[indexExercice]
                                                 }
-                                            }))
                                         }
                                         else{ delete seances[indexSeance].exercices[indexExercice] }
                                     }))
