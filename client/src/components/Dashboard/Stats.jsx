fe1929ae2436f5c3c955329ca02119d60efd26ad
AmaurySimondet <amaury.simondet@hotmail.com>
2022-10-24 11:18:07 +0200

    essaie d'ajouter elastique sur stats

--- client/src/components/Dashboard/Stats.jsx
+++ client/src/components/Dashboard/Stats.jsx
@@ -27,6 +27,9 @@ const CustomTooltip = ({ active, payload, label }) => {
             if (payload.dataKey === "repsTime"){
                 return (<p className="desc"> Valeur classification : {payload.value}</p>)
             }
+            if(payload.datakey === "exercices[0].Categories[0].estimation" || payload.datakey === "exercices[0].Categories[1].estimation" || payload.datakey === "exercices[0].Categories[2].estimation" || payload.datakey === "exercices[0].Categories[3].estimation" || payload.datakey === "exercices[0].Categories[4].estimation"{
+                return (<p className="desc"> Elastique estimation (kg) : {payload.value}</p>)
+            }
         })}
       </div>
     );
