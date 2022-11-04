0e495765f573cbc3b01553feea9a839d2bc9b619
AmaurySimondet <amaury.simondet@hotmail.com>
2022-11-04 18:01:15 +0100

    dep1

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const headers = {
   "Content-Type": "application/json"
 };
-const burl = "http://localhost:8800" // http://localhost:8800 https://prograrmorprealpha1.herokuapp.com
+const burl = "https://prograrmorprealpha1.herokuapp.com" // http://localhost:8800 https://prograrmorprealpha1.herokuapp.com
 
 export default {
 
