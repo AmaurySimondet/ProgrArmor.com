289e60cdd637ab42514e0e53571e723b04be7659
AmaurySimondet <amaury.simondet@hotmail.com>
2022-11-29 11:00:34 +0100

    ajout de la culotte lol

--- client/src/components/Dashboard/ProgrammeCard.jsx
+++ client/src/components/Dashboard/ProgrammeCard.jsx
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 function ProgrammeCard(props) {
     const [programme, setProgramme] = useState(props.programme);
@@ -6,6 +7,11 @@ function ProgrammeCard(props) {
         height: window.innerHeight,
         width: window.innerWidth
     });
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    function handleFlip() {
+        setIsFlipped(!isFlipped);
+    }
 
     useEffect(() => {
         function handleResize() {
@@ -23,81 +29,85 @@ function ProgrammeCard(props) {
     });
 
     return (
-        <div className="programme-card">
-            <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
-                <h1 style={{ marginBottom: "1rem", marginTop: "20px" }}>{programme.titre ? programme.titre : "Programme sans titre"} </h1>
-                <p>{programme.description ? programme.description : "/"} </p>
-                <p>{programme.id}</p>
-            </div>
+        <div className="react-card">
+            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+                <div className="YOUR_FRONT_CCOMPONENT" onClick={handleFlip}>
+                    <div className="programme-card">
+                        <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
+                            <h1 style={{ marginBottom: "1rem", marginTop: "20px" }}>{programme.titre ? programme.titre : "Programme sans titre"} </h1>
+                            <p>{programme.description ? programme.description : "/"} </p>
+                            <p>{programme.id}</p>
+                        </div>
 
-            <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
-                <p>{programme.type}</p>
-                <p>{programme.niveau}</p>
-                <p>{programme.seancesSemaine}</p>
-                <p>{programme.duree}</p>
-                {programme.materiel.map((materiel, index) => {
-                    if (index <= 3) {
-                        return <p style={{ overflow: "hidden", margin: "0" }}>{materiel.slice(0, 40)}</p>
-                    }
-                    if (index === 4) {
-                        return <p style={{ overflow: "hidden", margin: "0" }}>...</p>
-                    }
-                }
-                )}
-            </div>
+                        <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
+                            <p>{programme.type}</p>
+                            <p>{programme.niveau}</p>
+                            <p>{programme.seancesSemaine}</p>
+                            <p>{programme.duree}</p>
+                            {programme.materiel.map((materiel, index) => {
+                                if (index <= 3) {
+                                    return <p style={{ overflow: "hidden", margin: "0" }}>{materiel.slice(0, 40)}</p>
+                                }
+                                if (index === 4) {
+                                    return <p style={{ overflow: "hidden", margin: "0" }}>...</p>
+                                }
+                            }
+                            )}
+                        </div>
 
-            <i style={dimensions.width < 850 ? { fontSize: "5px" } : { fontSize: "10px" }}>
-                Cliquez sur le programme pour en savoir plus
-            </i>
+                        <i style={dimensions.width < 850 ? { fontSize: "5px" } : { fontSize: "10px" }}>
+                            Cliquez sur le programme pour en savoir plus
+                        </i>
 
-            {/* PROFILE */}
-            <footer style={dimensions.width > 850 ? { position: "absolute", bottom: "20px" } : { position: "absolute", bottom: "5px" }} className="programme-card-profile">
-                <table className="basic-table">
-                    <tr>
-                        <td>
-                            <img
-                                className="profile-pic"
-                                src="https://i0.wp.com/j19agriculture.com/wp-content/uploads/2020/06/ProfilePic.jpg?ssl=1"
-                                alt="profile-pic"
-                            />
-                        </td>
-                        <td>
-                            <p style={{ margin: "auto", display: "inline-block" }}>Jean Michel</p>
-                            <br />
-                            <p style={{ margin: "auto", display: "inline-block" }}>123456</p>
-                        </td>
-                    </tr>
-                </table>
+                        {/* PROFILE */}
+                        <footer style={dimensions.width > 850 ? { position: "absolute", bottom: "20px" } : { position: "absolute", bottom: "5px" }} className="programme-card-profile">
+                            <table className="basic-table">
+                                <tr>
+                                    <td>
+                                        <img
+                                            className="profile-pic"
+                                            src="https://i0.wp.com/j19agriculture.com/wp-content/uploads/2020/06/ProfilePic.jpg?ssl=1"
+                                            alt="profile-pic"
+                                        />
+                                    </td>
+                                    <td>
+                                        <p style={{ margin: "auto", display: "inline-block" }}>Jean Michel</p>
+                                        <br />
+                                        <p style={{ margin: "auto", display: "inline-block" }}>123456</p>
+                                    </td>
+                                </tr>
+                            </table>
 
-                {/* LIKE COMMENTS */}
-                <table className="basic-table">
-                    <tr>
-                        <td>
-                            <img className="small-img" src={require('../../images/icons/like.png')} alt='like' />
-                        </td>
-                        <td>
-                            <p style={{ margin: "auto" }}> 0 </p>
-                        </td>
-                        <td>
-                            <img className="small-img" src={require('../../images/icons/comment.png')} alt='comment' />
-                        </td>
-                        <td>
-                            <p style={{ margin: "auto" }}> 0 </p>
-                        </td>
-                    </tr>
-                </table>
-            </footer>
+                            {/* LIKE COMMENTS */}
+                            <table className="basic-table">
+                                <tr>
+                                    <td>
+                                        <img className="small-img" src={require('../../images/icons/like.png')} alt='like' />
+                                    </td>
+                                    <td>
+                                        <p style={{ margin: "auto" }}> 0 </p>
+                                    </td>
+                                    <td>
+                                        <img className="small-img" src={require('../../images/icons/comment.png')} alt='comment' />
+                                    </td>
+                                    <td>
+                                        <p style={{ margin: "auto" }}> 0 </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </footer>
 
-
-            {/* 
-            <img
-                style={{ width: "20px" }}
-                src={require('../../images/icons/flip.png')}
-                alt="flip"
-            /> */}
+                    </div >
+                </div>
 
 
-        </div >
+                <div className="YOUR_BACK_COMPONENT" onClick={handleFlip}>
+                    <div className="programme-card">
+                        <h1>Yo</h1>
+                    </div>
+                </div>
+            </ReactCardFlip>
+        </div>
     )
 };
 
--- client/src/components/Dashboard/ProgrammeCardExplication.jsx
+++ client/src/components/Dashboard/ProgrammeCardExplication.jsx
@@ -1,10 +1,16 @@
 import { React, useState, useEffect } from 'react';
+import ReactCardFlip from "react-card-flip";
 
 function ProgrammeCard(props) {
     const [dimensions, setDimensions] = useState({
         height: window.innerHeight,
         width: window.innerWidth
     });
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    function handleFlip() {
+        setIsFlipped(!isFlipped);
+    }
 
     useEffect(() => {
         function handleResize() {
@@ -22,69 +28,74 @@ function ProgrammeCard(props) {
     });
 
     return (
-        <div className="programme-card programme-card-explication">
-            <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
-                <h1 style={{ marginBottom: "1rem", marginTop: "20px" }}> Titre </h1>
-                <p> Description </p>
-                <p> ID du programme</p>
-                {dimensions.width < 850 ? <p> ... </p> : null}
-            </div>
+        <div className="react-card">
+            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+                <div className="YOUR_FRONT_CCOMPONENT" onClick={handleFlip}>
+                    <div className="programme-card programme-card-explication">
+                        <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
+                            <h1 style={{ marginBottom: "1rem", marginTop: "20px" }}> Titre </h1>
+                            <p> Description </p>
+                            <p style={{ margin: "0" }}> ID du programme</p>
+                            <p style={{ margin: "0" }}> ... </p>
+                        </div>
 
-            <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
-                <p> Type de programme </p>
-                <p> Niveau conseillé </p>
-                <p> Séances / semaine</p>
-                <p> Durée max d'une séance</p>
-                <p> Materiel nécessaire</p>
-            </div>
+                        <div style={dimensions.width > 850 ? { marginBottom: "40px" } : { marginBottom: "10px" }}>
+                            <p> Type de programme </p>
+                            <p> Niveau conseillé </p>
+                            <p> Séances / semaine</p>
+                            <p> Durée max d'une séance</p>
+                            <p> Materiel nécessaire</p>
+                        </div>
 
-            <i style={dimensions.width < 850 ? { fontSize: "5px" } : { fontSize: "10px" }}>
-                Cliquez sur le programme pour en savoir plus
-            </i>
+                        <i style={dimensions.width < 850 ? { fontSize: "5px" } : { fontSize: "10px" }}>
+                            Cliquez sur le programme pour en savoir plus
+                        </i>
 
-            {/* PROFILE */}
-            <footer style={dimensions.width > 850 ? { position: "absolute", bottom: "20px" } : { position: "absolute", bottom: "5px" }} className="programme-card-profile">
-                <table className="basic-table">
-                    <tr>
-                        <td>
-                            <img className="unknown-profile-pic profile-pic" src={require('../../images/profilepic.png')} alt='unknown-profile-pic' />
-                        </td>
-                        <td>
-                            <p style={{ margin: "auto", display: "inline-block" }}>Prénom Nom</p>
-                            <br />
-                            <p style={{ margin: "auto", display: "inline-block" }}>ID créateur</p>
-                        </td>
-                    </tr>
-                </table>
+                        {/* PROFILE */}
+                        <footer style={dimensions.width > 850 ? { position: "absolute", bottom: "20px" } : { position: "absolute", bottom: "5px" }} className="programme-card-profile">
+                            <table className="basic-table">
+                                <tr>
+                                    <td>
+                                        <img className="unknown-profile-pic profile-pic" src={require('../../images/profilepic.png')} alt='unknown-profile-pic' />
+                                    </td>
+                                    <td>
+                                        <p style={{ margin: "auto", display: "inline-block" }}>Prénom Nom</p>
+                                        <br />
+                                        <p style={{ margin: "auto", display: "inline-block" }}>ID créateur</p>
+                                    </td>
+                                </tr>
+                            </table>
 
-                {/* LIKE COMMENTS */}
-                <table className="basic-table">
-                    <tr>
-                        <td>
-                            <img className="small-img" src={require('../../images/icons/like.png')} alt='like' />
-                        </td>
-                        <td>
-                            <p style={{ margin: "auto", fontSize: "50%" }}> Nbre de likes </p>
-                        </td>
-                        <td>
-                            <img className="small-img" src={require('../../images/icons/comment.png')} alt='comment' />
-                        </td>
-                        <td>
-                            <p style={{ margin: "auto", fontSize: "50%" }}> Nbre de commentaires </p>
-                        </td>
-                    </tr>
-                </table>
-            </footer >
+                            {/* LIKE COMMENTS */}
+                            <table className="basic-table">
+                                <tr>
+                                    <td>
+                                        <img className="small-img" src={require('../../images/icons/like.png')} alt='like' />
+                                    </td>
+                                    <td>
+                                        <p style={{ margin: "auto", fontSize: "50%" }}> Nbre de likes </p>
+                                    </td>
+                                    <td>
+                                        <img className="small-img" src={require('../../images/icons/comment.png')} alt='comment' />
+                                    </td>
+                                    <td>
+                                        <p style={{ margin: "auto", fontSize: "50%" }}> Nbre de commentaires </p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </footer >
+                    </div>
+                </div>
 
+                <div className="YOUR_BACK_COMPONENT" onClick={handleFlip}>
+                    <div className="programme-card programme-card-explication">
+                        <div>
 
-            {/* 
-            <img
-                style={{ width: "20px" }}
-                src={require('../../images/icons/flip.png')}
-                alt="flip"
-            /> */}
-
-
+                            <img className="panties" src={require('../../images/other/panties.png')} alt='panties' />
+                        </div>
+                    </div>
+                </div>
+            </ReactCardFlip >
         </div >
     )
 };
